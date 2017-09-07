831c5ea6ae6c45cd094003120661d1e3c039056c
jz03 <jizhou03@gmail.com>
2017-09-07 22:46:08 +0800

    【添加新功能】 能够搜索文件夹

--- Thinking-in-java/src/String/regex/JGrep308.java
+++ Thinking-in-java/src/String/regex/JGrep308.java
@@ -1,5 +1,6 @@
 package String.regex;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -13,15 +14,26 @@ public class JGrep308 {
 			System.out.println("错误：重新输入参数");
 			System.exit(0);
 		}
+		File file = new File(args[0]);
+		File[] files = null;
+		if(file.isDirectory())
+			files = file.listFiles();
+		else
+			files = new File[]{new File(args[0])};
+		
 		System.out.println(Arrays.toString(args));
 		Pattern p = Pattern.compile(args[1]);
 		Matcher m = p.matcher("");
 		int index = 0;
-		for(String line : new TextFile(args[0])){
-			m.reset(line);
-			while(m.find()){
-				System.out.println(index++ +": "+m.group()+": "+m.start()+m.end());
+		for(File f : files){
+			System.out.println("--"+f.getName()+"--");
+			for(String line:new TextFile(f.getAbsolutePath())){
+				m.reset(line);
+				while(m.find()){
+					System.out.println(index++ +": "+m.group()+": "+m.start()+m.end());
+				}
 			}
+			
 		}
 	}
 

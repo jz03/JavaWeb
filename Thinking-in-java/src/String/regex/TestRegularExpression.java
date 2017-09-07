950d70aeaa4c64fa94c8e1a08f4cc48c1e334991
jz03 <jizhou03@gmail.com>
2017-09-07 21:34:31 +0800

    添加 文件的操作

--- Thinking-in-java/src/String/regex/TestRegularExpression.java
+++ Thinking-in-java/src/String/regex/TestRegularExpression.java
@@ -1,23 +1,27 @@
 package String.regex;
 
-import java.util.regex.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+/*
+ *测试正则表达式
+ */
 public class TestRegularExpression {
-  public static void main(String[] args) {
-    if(args.length < 2) {
-      System.out.println("Usage:\njava TestRegularExpression " +
-        "characterSequence regularExpression+");
-      System.exit(0);
-    }
-    System.out.println("Input: \"" + args[0] + "\"");
-    for(String arg : args) {
-    	System.out.println("Regular expression: \"" + arg + "\"");
-      Pattern p = Pattern.compile(arg);
-      Matcher m = p.matcher(args[0]);
-      while(m.find()) {
-    	  System.out.println("Match \"" + m.group() + "\" at positions " +
-          m.start() + "-" + (m.end() - 1));
-      }
-    }
-  }
+	public static void main(String[] args) {
+		if (args.length < 2) {
+			System.out.println("Usage:\njava TestRegularExpression "
+		+ "characterSequence regularExpression+");
+			System.exit(0);
+		}
+		System.out.println("Input: \"" + args[0] + "\"");
+		for (String arg : args) {
+			System.out.println("Regular expression: \"" + arg + "\"");
+			Pattern p = Pattern.compile(arg);
+			Matcher m = p.matcher(args[0]);
+			while (m.find()) {
+				System.out.println("Match \"" + m.group() + "\" at positions " 
+			+ m.start() + "-" + (m.end() - 1));
+			}
+		}
+	}
 }

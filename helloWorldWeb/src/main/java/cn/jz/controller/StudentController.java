58c95a668b3c30471f514dae119331c3e07d8d19
jz03 <jizhou03@gmail.com>
2018-04-17 19:41:44 +0800

    helloWorldWeb  错误与异常的处理

--- helloWorldWeb/src/main/java/cn/jz/controller/StudentController.java
+++ helloWorldWeb/src/main/java/cn/jz/controller/StudentController.java
@@ -2,12 +2,14 @@ package cn.jz.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import cn.jz.bean.Student;
+import cn.jz.exception.SpringException;
 
 @Controller
 public class StudentController {
@@ -17,9 +19,18 @@ public class StudentController {
 	}
 
 	@RequestMapping(value = "/addStudent", method = RequestMethod.POST)
+	@ExceptionHandler({SpringException.class})
 	public String addStudent( Student student, ModelMap model) {
-		model.addAttribute("name", student.getName());
-		model.addAttribute("age", student.getAge());
+		if (student.getName().length() < 5) {
+			throw new SpringException("Given name is too short");
+		} else {
+			model.addAttribute("name", student.getName());
+		}
+		if (student.getAge() < 10) {
+			throw new SpringException("Given age is too low");
+		} else {
+			model.addAttribute("age", student.getAge());
+		}
 		model.addAttribute("id", student.getId());
 		return "result";
 	}
---
+++ helloWorldWeb/src/main/webapp/WEB-INF/jsp/error.jsp
@@ -0,0 +1,11 @@
+<%@taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
+<html>
+<head>
+    <title>Spring Error Page</title>
+</head>
+<body>
+
+<p>An error occured, please contact webmaster.</p>
+
+</body>
+</html>
